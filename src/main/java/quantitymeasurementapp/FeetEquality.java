5bd70f784334221dd2171b9f6011e6c004da1c78
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-02-20 15:34:45 +0530

    [Shriyansh] : added feetequality file

--- src/main/java/quantitymeasurementapp/FeetEquality.java
+++ src/main/java/quantitymeasurementapp/FeetEquality.java
@@ -2,4 +2,30 @@ package quantitymeasurementapp;
 
 public class FeetEquality {
 
+    private final double value;
+
+    public FeetEquality(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj)
+            return true;
+
+        if (obj == null)
+            return false;
+
+        if (getClass() != obj.getClass())
+            return false;
+
+        FeetEquality other = (FeetEquality) obj;
+
+        return Double.compare(this.value, other.value) == 0;
+    }
 }
