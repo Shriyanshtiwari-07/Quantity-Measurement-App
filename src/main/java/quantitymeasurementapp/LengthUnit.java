a0a8f11bd8117da15b8e5444d1d8cf8af7d789f2
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-02-24 14:08:55 +0530

    [Shriyansh] : added lengthunit file

--- src/main/java/quantitymeasurementapp/LengthUnit.java
+++ src/main/java/quantitymeasurementapp/LengthUnit.java
@@ -1,7 +1,7 @@
 package quantitymeasurementapp;
 
 
-public enum LengthUnit{
+public enum LengthUnit implements IMeasurable{
 	FEET(1.0),
     INCHES(1.0 / 12.0),
     YARD(3.0),
@@ -22,4 +22,11 @@ public enum LengthUnit{
     public double convertFromBaseUnit(double value) {
     	return (LengthUnit.FEET.getConversionFactor()*value)/this.getConversionFactor();
     }
+
+
+	@Override
+	public String getUnitName() {
+		// TODO Auto-generated method stub
+		return LengthUnit.this.name();
+	}
 }
