f94b5140048442defa967ee452e7f39e16f9ef8d
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-03-17 12:56:25 +0530

    [Shriyansh]: added IQuantityMeasurement file

---
+++ src/main/java/measure/reposistory/IQuantityMeasurementRepository.java
@@ -0,0 +1,10 @@
+package measure.reposistory;
+
+
+import measure.model.QuantityMeasurementEntity;
+import java.util.*;
+
+public interface IQuantityMeasurementRepository {
+      void save(QuantityMeasurementEntity enity);
+      List<QuantityMeasurementEntity> getAllMeasurement();
+}
